4a2c8f880c880cd6c3db1b121001724ed2027a0f
realonbebeto <projobs254@gmail.com>
2022-08-08 16:53:11 +0300

    front end refine+404

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
 
 from .routers import anga
+
+BASE_PATH = Path(__file__).resolve().parent
+templates = Jinja2Templates(
+    directory=str(BASE_PATH)+"/templates/", autoescape=False, auto_reload=True)
 
 app = FastAPI()
 
@@ -17,3 +22,11 @@ app.add_middleware(
 
 
 app.include_router(anga.router, prefix='')
+
+
+@app.exception_handler(404)
+async def custom_404_handler(request, __):
+    res = {"title": "Error Page",
+           "error": "Page Not Found",
+           "name": "Bebeto Nyamwamu"}
+    return templates.TemplateResponse("404.html", {"request": request, "res": res})
--- app/routers/anga.py
+++ app/routers/anga.py
@@ -12,37 +12,43 @@ templates = Jinja2Templates(
     directory=str(BASE_PATH)+"/../templates/", autoescape=False, auto_reload=True)
 
 
-@router.get("/index")
-def getIndex():
-    return {"title": "Weather App", "name": "Bebeto Nyamwamu"}
+@router.get("/")
+def getAbout(request: Request):
+    res = {"title": "Weather App", "name": "Bebeto Nyamwamu"}
+    return templates.TemplateResponse("index.html", context={"request": request, "res": res})
 
 
 @router.get("/about")
-def getAbout():
-    return {"title": "Weather App", "name": "Bebeto Nyamwamu"}
+def getAbout(request: Request):
+    res = {"title": "Weather App", "name": "Bebeto Nyamwamu"}
+    return templates.TemplateResponse("about.html", context={"request": request, "res": res})
 
 
 @router.get("/help")
-def getHelp():
-    return {"title": "Help You", "name": "Bebeto Nyamwamu", "message": "Welcome, How may I help you"}
+def getHelp(request: Request):
+    res = {"title": "Help You", "name": "Bebeto Nyamwamu",
+           "message": "Welcome, How may I help you"}
+    return templates.TemplateResponse("help.html", context={"request": request, "res": res})
 
 
 @router.get("/weather")
 def getWeather(request: Request):
-    return templates.TemplateResponse("weather.html", context={"request": request})
+    res = {"name": "Bebeto Nyamwamu"}
+    return templates.TemplateResponse("weather.html", context={"request": request, "res": res})
 
 
 @router.post("/weather", response_class=HTMLResponse)
 def getWeather(request: Request, query_loc: str = Form(...)):
     cast_response, place_name = foreCast(**geoCode(query_loc))
-    weather_info = {"place_name": place_name,
-                    "time": cast_response['current']['observation_time'],
-                    "desc": cast_response['current']['weather_descriptions'][0],
-                    "temp": cast_response['current']['temperature'],
-                    "feels_like_temp": cast_response['current']['feelslike'],
-                    "ccover": cast_response['current']['cloudcover'],
-                    "rain": cast_response['current']['precip'],
-                    "humidity": cast_response['current']['humidity'],
-                    "swind": cast_response['current']['wind_speed'],
-                    "icons": cast_response['current']['weather_icons'][0]}
-    return templates.TemplateResponse("weather.html", context={"request": request, "weather_info": weather_info})
+    res = {"place_name": place_name,
+           "time": cast_response['current']['observation_time'],
+           "desc": cast_response['current']['weather_descriptions'][0],
+           "temp": cast_response['current']['temperature'],
+           "feels_like_temp": cast_response['current']['feelslike'],
+           "ccover": cast_response['current']['cloudcover'],
+           "rain": cast_response['current']['precip'],
+           "humidity": cast_response['current']['humidity'],
+           "swind": cast_response['current']['wind_speed'],
+           "icons": cast_response['current']['weather_icons'][0],
+           "name": "Bebeto Nyamwamu"}
+    return templates.TemplateResponse("weather.html", context={"request": request, "res": res})
